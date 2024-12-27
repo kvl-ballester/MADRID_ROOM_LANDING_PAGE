85913890a5fc7c4ef97ef88b0cd585dc36a13ab5
kvl-ballester <kvl.ballester@gmail.com>
2024-12-27 13:27:27 +0100

    add title and reviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useTranslation } from 'react-i18next'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Tittle from './components/Tittle'
+import ReviewCarousel from './components/ReviewCarousel'
 
 function App() {
   const {t, i18n} = useTranslation()
 
   return (
     <div className="landing-page">
-      <h1>{t('welcome')}</h1>
+      <Header />
+      <div className="content">
+        <Tittle />
+        <ReviewCarousel />
+      </div>
+      <Footer />
     </div>
   )
 }
